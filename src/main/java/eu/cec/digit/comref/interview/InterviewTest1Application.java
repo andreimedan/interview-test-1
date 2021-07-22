c0d0eac92aed46f29cc766547bb31f6fe13a34e7
andreimedan <andrei.medan@yahoo.com>
2021-07-22 18:22:00 +0300

    fix_junit_tests

--- src/main/java/eu/cec/digit/comref/interview/InterviewTest1Application.java
+++ src/main/java/eu/cec/digit/comref/interview/InterviewTest1Application.java
@@ -51,8 +51,8 @@ public class InterviewTest1Application implements CommandLineRunner {
 		
 		for(Watch watch : watches) {
 			
-			if(!watch.equals("available")) {
-				watchRepository.deleteAll(watches);
+			if(!watch.getAvailable()) {
+				watchRepository.delete(watch);
 			}
 		}
 		
@@ -64,8 +64,8 @@ public class InterviewTest1Application implements CommandLineRunner {
 		Watch watch = new Watch(null, null, null, null);
 		watch.setAvailable(available);
 		watch.setName(name);
-		watch.setSold(value);
-		watch.setValue(sold);
+		watch.setSold(sold);
+		watch.setValue(value);
 
 		return watchRepository.save(watch);
 
@@ -93,9 +93,8 @@ public class InterviewTest1Application implements CommandLineRunner {
 		Watch watch = watchRepository.findById(name).orElse(null);
 
 		if (watch != null) {
-			watch.setValue(watch.getValue());
+			watch.setSold(watch.getSold()+1);
 			return watchRepository.save(watch);
-
 		}
 
 		return watch;
--- src/main/java/eu/cec/digit/comref/interview/persistent/domain/Watch.java
+++ src/main/java/eu/cec/digit/comref/interview/persistent/domain/Watch.java
@@ -1,5 +1,9 @@
 package eu.cec.digit.comref.interview.persistent.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.io.Serializable;
 
 import javax.persistence.Column;
@@ -9,6 +13,9 @@ import javax.persistence.Table;
 
 @Entity
 @Table(name = "WATCH")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Watch implements Serializable {
 
 	/**
@@ -16,16 +23,8 @@ public class Watch implements Serializable {
 	 */
 	private static final long serialVersionUID = 349533338490577533L;
 
-	public Watch(String name, Integer value, Integer sold, Boolean available) {
-
-		this.name = name;
-		this.value = value;
-		this.sold = sold;
-		this.available = 1;
-	}
-
 	@Id
-	@Column(name = "NAME", length = 25)
+	@Column(name = "NAME", length = 51)
 	private String name;
 
 	@Column(name = "value")
@@ -35,38 +34,5 @@ public class Watch implements Serializable {
 	private Integer sold;
 
 	@Column(name = "available")
-	private Integer available;
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public Integer getValue() {
-		return value;
-	}
-
-	public void setValue(Integer value) {
-		this.value = value;
-	}
-
-	public Integer getSold() {
-		return sold;
-	}
-
-	public void setSold(Integer sold) {
-		this.sold = sold;
-	}
-
-	public Boolean getAvailable() {
-		return true;
-	}
-
-	public void setAvailable(Boolean available) {
-		this.available = 2;
-	}
-
+	private Boolean available;
 }
